0876d9a2eca53ed51f51cf9d5b44994c471371ad
Juha Lundan <juha.lundan@gmail.com>
2023-12-13 13:45:27 +0200

    chore: clean out old files

--- convert-dataset.js
+++
@@ -1,3 +0,0 @@
-const {Dataset} = require("./dataset");
-const dataSet = new Dataset('entryId', 'context', 'en');
-dataSet.loadFromXlsx(process.argv[2]).filterEmptyValues().writeToJsonFile(process.argv[3], true);
